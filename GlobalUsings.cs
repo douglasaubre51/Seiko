925f4807cf0865356b0bc3660e80d383cd8fb7d8
allen <boseallen192@gmail.com>
2025-06-28 12:43:13 +0530

    cleared built in templates

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,6 +1,1 @@
 global using Fonts;
-global using Seiko.Data;
-global using Seiko.PageModels;
-global using Seiko.Pages;
-global using Seiko.Services;
-global using Seiko.Utilities;
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -22,22 +22,9 @@ namespace Seiko
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
-    		builder.Services.AddLogging(configure => configure.AddDebug());
+            builder.Logging.AddDebug();
+            builder.Services.AddLogging(configure => configure.AddDebug());
 #endif
-
-            builder.Services.AddSingleton<ProjectRepository>();
-            builder.Services.AddSingleton<TaskRepository>();
-            builder.Services.AddSingleton<CategoryRepository>();
-            builder.Services.AddSingleton<TagRepository>();
-            builder.Services.AddSingleton<SeedDataService>();
-            builder.Services.AddSingleton<ModalErrorHandler>();
-            builder.Services.AddSingleton<MainPageModel>();
-            builder.Services.AddSingleton<ProjectListPageModel>();
-            builder.Services.AddSingleton<ManageMetaPageModel>();
-
-            builder.Services.AddTransientWithShellRoute<ProjectDetailPage, ProjectDetailPageModel>("project");
-            builder.Services.AddTransientWithShellRoute<TaskDetailPage, TaskDetailPageModel>("task");
 
             return builder.Build();
         }
